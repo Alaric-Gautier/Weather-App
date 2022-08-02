1391a54ce5d4491dd1c458be9610cf180d24d55c
Alaric-Gautier <92737965+Alaric-Gautier@users.noreply.github.com>
2022-08-02 14:32:40 +0200

    add a background image

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'e52a5651e2msh0f1105ffdeb33d2p19cbc6jsn3bbe8d8b2aef',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
