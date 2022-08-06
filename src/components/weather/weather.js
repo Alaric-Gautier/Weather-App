7bb3beb1dfed21b1c9169f0562a0d935d1582e8f
Alaric-Gautier <92737965+Alaric-Gautier@users.noreply.github.com>
2022-08-06 16:55:37 +0200

    dynamization and weather component finished

--- src/components/weather/weather.js
+++ src/components/weather/weather.js
@@ -11,26 +11,26 @@ const Weather = ({data}) => {
                 <img src={`icons/${data.weather[0].icon}.png`} alt="weather" className="weather-icon"/>
             </div>
             <div className="bottom">
-                <p className="temperature">{data.main.temp}°C</p>
+                <p className="temperature">{Math.round(data.main.temp)}°C</p>
                 <div className="details">
                     <div className="parameter-row">
                         <span className="parameter-label">Details</span>
                     </div>
                     <div className="parameter-row">
                         <span className="parameter-label">Feels like</span>
-                        <span className="parameter-value">22°C</span>
+                        <span className="parameter-value">{Math.round(data.main.feels_like)}°C</span>
                     </div>
                     <div className="parameter-row">
                         <span className="parameter-label">Wind</span>
-                        <span className="parameter-value">2 km/h</span>
+                        <span className="parameter-value">{Math.round(data.wind.speed * 3.6)} km/h</span>
                     </div>
                     <div className="parameter-row">
                         <span className="parameter-label">Humidity</span>
-                        <span className="parameter-value">20%</span>
+                        <span className="parameter-value">{data.main.humidity}%</span>
                     </div>
                     <div className="parameter-row">
                         <span className="parameter-label">Pressure</span>
-                        <span className="parameter-value">15 hPa</span>
+                        <span className="parameter-value">{data.main.pressure} hPa</span>
                     </div>
                 </div>
             </div>
